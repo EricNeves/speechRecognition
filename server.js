ae6c3bac6dbb13d0de39e8efd2ba4e77145c85b0
Eric Neves Rodrigues <ericnevesrodrigues@gmail.com>
2023-03-05 18:16:14 -0400

    Add server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path    = require("path")
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(
+    express.static(path.resolve(__dirname, 'dist'))
+)
+
+app.get('/', (req, res) => {
+    res.sendFile(
+        path.resolve(__dirname, 'dist', 'index.html')
+    )
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`)
+})
